517099e285ed8aa32066e5e7227b5f5a3068f01b
SHEHAN22000 <shehanmsuv@gmail.com>
2023-02-15 17:52:55 +0200

    Working on Universal Header and Footer

---
+++ index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bon A p p é t i t - Team-13</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="homestyle.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Encode+Sans:wght@400;700&display=swap" rel="stylesheet">
+
+</head>
+  <body>
+
+  <div class="wrapper">
+  <div class="container">
+
+      <div class="row">
+        <div class="col-md-2">
+          <img class="logo" src="images/logo.png" alt="logo">
+        </div>
+
+        <div class="col-md-6">
+          <h1><b>Bon Appétit</b></h1>
+        </div>
+
+        <div class="col-md-4" style="text-align: right;">
+          <a class="btn btn-danger" href="about.html" role="button" style="vertical-align: middle ;"> <b>About us</b></a>
+          <a class="btn btn-danger" href="https://www.hamk.fi/" role="button" style="vertical-align: middle ;"> <b>Make Reservation</b></a>
+          <a class="home" href="index.html"><img class="home" src="images/home.png" alt="home button"> </a>
+        </div>
+
+<h2>There is always something for everyone.</h2>
+      </div>
+
+      <div class="row2">
+        <div class="col-md-8">
+          <p>Bon Appétit is located in the historic premises of the Swedish Theater in Helsinki. Here we serve, among other things, classic dishes such as Biff Rydberg, a classic Skagen, Skaldjursplatå and other delicacies. We invest in high-quality service, good drinks and good ingredients. We also manage the intermission service at the Swedish Theatre, which you can book in advance.</p>         
+          <p>At Bon Appétit we serve grilled premium hamburgers and kebabs with good accessories and top quality ingredients. We invest in small-scale preparation and cooking, freshly baked bread and the house's own dressings.</p>
+        </div>
+      </div>
+
+      <div class="row3">
+        <a class="card1" href="break.html">
+          <img src="images/breakfast.jpg" alt="breakfast menu" style="width:90%">
+          <div class="container">
+            <br>
+            <h4><b>BREAKFAST</b></h4>
+          </div>
+        </a>
+
+        <a class="card2" href="lunch.html">
+          <img src="images/lunch.jpg" alt="lunch menu" style="width:90%">
+          <div class="container">
+            <br>
+            <h4><b>LUNCH</b></h4>
+          </div>
+        </a>
+
+        <a class="card3" href="dinner.html">
+          <img src="images/dinner.jpg" alt="dinner menu" style="width:90%">
+          <div class="container">
+            <br>
+            <h4><b>DINNER</b></h4>
+          </div>
+        </a>
+
+      </div>
+
+      <footer>
+          <div class="footer-box">
+
+            <div>
+              <P>&copy; TEAM-13 - HAMK 2023<br>
+                 Vankanlähde 9, 13100 Hämeenlinna.</P>
+            </div>
+            
+            <div>
+              <P>Email: bon.appétit@gmail.fi</P>
+            </div>
+
+            <div>
+              <P>Tel: <a href="tel:+358101010101">+358 1010 10101</a></p>
+            </div>
+
+          </div>
+      </footer>
+    
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
