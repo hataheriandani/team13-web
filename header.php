a57a518e5e2ee29b0e43e34944fc4882ec13b5a6
SHEHAN22000 <shehanmsuv@gmail.com>
2023-02-15 22:33:08 +0200

    Header - test 3

--- header.php
+++ header.php
@@ -10,10 +10,10 @@
           <h1><b>Bon Appétit</b></h1>
         </div>
 
-        <div class="col-md-5" style="text-align: right;">
-          <a class="btn btn-danger" href="about.html" role="button" style="vertical-align: middle ;"> <b>About us</b></a>
-          <a class="btn btn-danger" href="about.html" role="button" style="vertical-align: middle ;"> <b>Products</b></a>
-          <a class="btn btn-danger" href="https://www.hamk.fi/" role="button" style="vertical-align: middle ;"> <b>Make Reservation</b></a>
-          <a class="btn btn-danger" href="https://www.hamk.fi/" role="button" style="vertical-align: middle ;"> <b>Feedback</b></a>
-          <a class="home" href="index.html"><img class="home" src="images/home.png" alt="home button"> </a>
+        <div class="col-md-6" style="text-align: right;">
+          <a class="btn btn-danger" href="#" role="button" style="vertical-align: middle ;"> <b>About us</b></a>
+          <a class="btn btn-danger" href="#" role="button" style="vertical-align: middle ;"> <b>Products</b></a>
+          <a class="btn btn-danger" href="#" role="button" style="vertical-align: middle ;"> <b>Make Reservation</b></a>
+          <a class="btn btn-danger" href="#" role="button" style="vertical-align: middle ;"> <b>Feedback</b></a>
+          <a class="home" href="index.php"><img class="home" src="images/home.png" alt="home button"> </a>
         </div>
