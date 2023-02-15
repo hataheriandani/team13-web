91d31169782fd80661ca90693f37673c599fafe9
SHEHAN22000 <shehanmsuv@gmail.com>
2023-02-15 21:29:04 +0200

    Header - Feedback and Products buttons added - Test 2

--- header.php
+++ header.php
@@ -2,16 +2,18 @@
 <html>
 <head>
 <div class="row">
-        <div class="col-md-2">
+        <div class="col-md-1">
           <img class="logo" src="images/logo.png" alt="logo">
         </div>
 
-        <div class="col-md-6">
+        <div class="col-md-5">
           <h1><b>Bon Appétit</b></h1>
         </div>
 
-        <div class="col-md-4" style="text-align: right;">
+        <div class="col-md-5" style="text-align: right;">
           <a class="btn btn-danger" href="about.html" role="button" style="vertical-align: middle ;"> <b>About us</b></a>
+          <a class="btn btn-danger" href="about.html" role="button" style="vertical-align: middle ;"> <b>Products</b></a>
           <a class="btn btn-danger" href="https://www.hamk.fi/" role="button" style="vertical-align: middle ;"> <b>Make Reservation</b></a>
+          <a class="btn btn-danger" href="https://www.hamk.fi/" role="button" style="vertical-align: middle ;"> <b>Feedback</b></a>
           <a class="home" href="index.html"><img class="home" src="images/home.png" alt="home button"> </a>
         </div>
