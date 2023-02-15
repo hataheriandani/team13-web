613eaded2f96e19efcf9a2f06a7d55275709c7a7
hataheriandani <120684795+hataheriandani@users.noreply.github.com>
2023-02-15 23:33:06 +0200

    In line 47, html was changed to php The footer was removed from the index

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
           </div>
         </a>
 
-        <a class="card3" href="dinner.html">
+        <a class="card3" href="dinner.php">
           <img src="images/dinner.jpg" alt="dinner menu" style="width:90%">
           <div class="container">
             <br>
@@ -53,25 +53,6 @@
         </a>
 
       </div>
-
-      <footer>
-          <div class="footer-box">
-
-            <div>
-              <P>&copy; TEAM-13 - HAMK 2023<br>
-                 Vankanlähde 9, 13100 Hämeenlinna.</P>
-            </div>
-            
-            <div>
-              <P>Email: bon.appétit@gmail.fi</P>
-            </div>
-
-            <div>
-              <P>Tel: <a href="tel:+358101010101">+358 1010 10101</a></p>
-            </div>
-
-          </div>
-      </footer>
     
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
