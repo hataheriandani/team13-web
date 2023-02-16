6034c9d14d207aa1f7faba718a6a06338b8b0e9c
SHEHAN22000 <shehanmsuv@gmail.com>
2023-02-16 23:41:00 +0200

    Index, Header and homestyle edited

--- header.php
+++ header.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
-<html>
-<head>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bon A p p é t i t - Team-13</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="homestyle.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Encode+Sans:wght@400;700&display=swap" rel="stylesheet">
+
 <div class="row">
         <div class="col-md-1">
           <img class="logo" src="images/logo.png" alt="logo">
@@ -13,16 +21,21 @@
         <div class="col-md-6" style="text-align: right;">
           <a class="btn btn-danger" href="#" role="button" style="vertical-align: middle ;"> <b>About us</b></a>
           <a class="btn btn-danger" href="#" role="button" style="vertical-align: middle ;"> <b>Products</b></a>
-          <a class="btn btn-danger" href="#" role="button" style="vertical-align: middle ;"> <b>Make Reservation</b></a>
           <a class="btn btn-danger" href="#" role="button" style="vertical-align: middle ;"> <b>Feedback</b></a>
           <a class="btn btn-danger" href="#" role="button" style="vertical-align: middle ;"> <b>collaboration request</b></a>
           <a class="home" href="index.php"><img class="home" src="images/home.png" alt="home button"> </a>
         </div>
-        <style>      
-    .logo {
 
-width: 200px;
-margin-top: 20px;
+
+<style>
+
+.logo {
+    width: 150px;
+    margin-top: 20px;
+    margin-bottom: 10px;
+    -webkit-filter: drop-shadow(5px 5px 5px #141516);
+    filter: drop-shadow(5px 5px 5px #141516);
+
 }
 
 .btn {
@@ -31,7 +44,7 @@ margin-top: 20px;
     color:#222629;
     border-radius: 3;
     align-self: center;
-    margin-top: 90px;
+    margin-top: 70px;
     margin-left: 5px;
     -webkit-filter: drop-shadow(5px 5px 5px #141516);
     filter: drop-shadow(5px 5px 5px #141516);
@@ -40,16 +53,28 @@ margin-top: 20px;
 h1 {
     text-align: left;
     margin-top: 60px;
-    margin-left: 10px;
+    margin-left: 50px;
     color: #ffde00;
-    font-size: 90px;
+    font-size: 80px;
     text-shadow: (5px 5px 5px #141516);
     filter: drop-shadow(5px 5px 5px #141516);
 }
 
+h2 {
+
+    font-family:Edwardian Script ITC;
+    font-size: 65px;
+    text-align: left;
+    margin-bottom: 20px;
+    text-shadow: (5px 5px 5px #141516);
+    filter: drop-shadow(5px 5px 5px #141516);
+
+}
+
+
 .home {
     width: 60px;
-    margin-top: 90px;
+    margin-top: 70px;
     -webkit-filter: drop-shadow(5px 5px 5px #141516);
     filter: drop-shadow(5px 5px 5px #141516);
 }
--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php include "header.php" ?>
 
 <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bon A p p é t i t - Team-13</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="homestyle.css">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Encode+Sans:wght@400;700&display=swap" rel="stylesheet">
-
-</head>
   <body>
 
   <div class="wrapper">
