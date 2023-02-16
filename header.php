a19b66d6995d10a8d63e5bd27f64c2c9a04222f0
SHEHAN22000 <shehanmsuv@gmail.com>
2023-02-16 12:39:56 +0200

    Header and Footer ready

--- header.php
+++ header.php
@@ -15,5 +15,48 @@
           <a class="btn btn-danger" href="#" role="button" style="vertical-align: middle ;"> <b>Products</b></a>
           <a class="btn btn-danger" href="#" role="button" style="vertical-align: middle ;"> <b>Make Reservation</b></a>
           <a class="btn btn-danger" href="#" role="button" style="vertical-align: middle ;"> <b>Feedback</b></a>
+          <a class="btn btn-danger" href="#" role="button" style="vertical-align: middle ;"> <b>collaboration request</b></a>
           <a class="home" href="index.php"><img class="home" src="images/home.png" alt="home button"> </a>
         </div>
+        <style>      
+    .logo {
+
+width: 200px;
+margin-top: 20px;
+}
+
+.btn {
+    background-color:#ffde00;
+    border-color:#ffde00;
+    color:#222629;
+    border-radius: 3;
+    align-self: center;
+    margin-top: 90px;
+    margin-left: 5px;
+    -webkit-filter: drop-shadow(5px 5px 5px #141516);
+    filter: drop-shadow(5px 5px 5px #141516);
+}
+
+h1 {
+    text-align: left;
+    margin-top: 60px;
+    margin-left: 10px;
+    color: #ffde00;
+    font-size: 90px;
+    text-shadow: (5px 5px 5px #141516);
+    filter: drop-shadow(5px 5px 5px #141516);
+}
+
+.home {
+    width: 60px;
+    margin-top: 90px;
+    -webkit-filter: drop-shadow(5px 5px 5px #141516);
+    filter: drop-shadow(5px 5px 5px #141516);
+}
+
+.home :hover {
+    transform: scale(1.1);
+}
+
+</style>
+</head>
