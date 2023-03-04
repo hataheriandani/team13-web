0fc1f81d34bdd3cf81b882da2c3850fa48b2ec1c
hataheriandani <hosseinali22000@student.hamk.fi>
2023-03-04 18:02:19 +0200

    According to the opinion of the group members, I changed the collaboration request form in such a way that it all looks like.

--- collaboration.php
+++ collaboration.php
@@ -1,55 +1,107 @@
-<?php
-$title = "Collaboration";
-include "header1.php";?>
+<?php include "header1.php" ?>
 
-<form action="" method="post" onsubmit="return crud()" name="form1">
-    <input type ="text" name ="fname" placeholder="First Name" required onblur="form1"> <br><br>
-    <input type ="text" name ="lname" placeholder="Last Name" required><br><br>
-    <input type ="text" name ="city" placeholder="City" required><br><br>
-    <input type ="text" name ="email" placeholder="Email Adrress" required><br><br>
-    <input type ="num" name ="pnumber" placeholder="Phone Number" required><br><br>
-    <select name="gender">
-        <option value="G1">male</option>
-        <option value="G2">fmale</option>
-    </select><br><br>
-    <select name="section">
-        <option value="Group1">Advertising</option>
-        <option value="Group2">Sale</option>
-        <option value="Group3">Cooking </option>
-    </select><br><br>
+<head>
+  
+    <link rel="stylesheet" href="collaborationstyle.css">
+</head>
 
-    <input type ="submit" value ="Submit" name="submit" >
+<div class="container">
 
-</form>
-
-<?php
-        if(isset($_POST['submit'])){
-            $fname=$_POST['fname'];
-            $lname=$_POST['lname'];
-            $city=$_POST['city'];
-            $email=$_POST['email'];
-            $pnumber=$_POST['pnumber'];
-            $groupid=$_POST['gender'];
-            $groupid=$_POST['section'];
-
-            include 'dbtaheri.php';
-            $sql="insert into studentInfu(fname,lname,city,groupid)
-            values('$fname','$lname','$city','$email','$pnumber', '$groupid')";
-            
-            if($conn->query($sql)===TRUE){
-                
-                echo"Your information is added successfully";
-
-            }
-            else{
-
-                echo"Error:" .$conn->error;
-
-            }
-        }
-?>
+<form method="POST" action="">
 
 
-<?php ?>
+<div class="row1">
+
+    <div class="col1">
+        <h3 class="title">Personal Information</h3>
+        <div class="inputBox">
+            <span> First Name :</span>
+            <input type="text" name="fname" placeholder="Enter your first name">
+        </div>
+
+        <div class="inputBox">
+            <span> Last Name :</span>
+            <input type="text" name="lname" placeholder="Enter your last name">
+        </div>
+
+        <div class="inputBox">
+            <span> Email:</span>
+            <input type="text" name="email" placeholder="Enter your email">
+        </div>
+
+        <div class="inputBox">
+            <span> Phone Number:</span>
+            <input type="number" name="phone" placeholder="Enter your phone number">
+        </div>
+        
+        <div class="flex">
+            <div class="inputBox">
+            <span> City:</span>
+            <input type="text" name="city" placeholder="Enter your city">
+        </div>
+        <div class="inputBox">
+            <span> Gender:</span>
+            <select name="gender" id="gender">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+            </select>
+        </div>
+
+        <div class="inputBox">
+            <span> Section:</span>
+            <select name="section" id="section">
+                <option value="IT">IT</option>
+                <option value="Marketing">Marketing</option>
+                <option value="Finance">Finance</option>
+                <option value="Human Resources">Human Resources</option>
+            </select>
+        </div>
+
+
+
+    </div>
+
+      
+
+    </div>
+
+    <div class="col1">
+        <h3 class="title">Resume Upload</h3>
+        <div class="inputBox">
+            <span>Upload your resume:</span>
+            <input type="file" name="resume" accept=".pdf,.doc,.docx">
+        </div>
+
+        <input type="submit" value="submit" class="submit-btn" name="submit">
+    </div>
+    </div>
+    </div>
+    </div>
+
+    <?php 
+    if (isset($_POST['submit'])){
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $city = $_POST['city'];
+        $gender = $_POST['gender'];
+        $section = $_POST['section'];
+       include 'db.php';
+      $sql = "INSERT INTO job_application(fname,lname,email,phone,city,gender,section)
+      VALUES('$fname','$lname','$email','$phone','$city','$gender','$section')";
+      
+
+      if ($conn ->query($sql)==TRUE){
+        echo "Your application has been submitted successfully";
+    }
+
+    else {
+        echo "Error:".$conn -> error;
+    }
+        
+    }
+    ?>
 
 <?php include "footer1.php" ?>
