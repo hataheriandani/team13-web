c28b759a2b8c8c971996fdb6123565a61bac9463
Abewickrama Disanayaka Shanaka Sanjeewa <114850380+ShanakaSanjeewa@users.noreply.github.com>
2023-03-07 10:39:55 +0200

    validate the form using js

--- productData.php
+++ productData.php
@@ -1,27 +1,23 @@
 <?php include "header1.php" ?>
 
 <head>
-  
-    <link rel="stylesheet" href="orderstyle.css">
+      <link rel="stylesheet" href="orderstyle.css">
 </head>
 
 <div class="container">
 
-<form method="POST" action="">
-
-
+<form method="POST" action="" name="form1" id="form1" onsubmit="return formValidation()" >
 <div class="row1">
-
     <div class="col1">
         <h3 class="title">Menu Detail</h3>
         <div class="inputBox">
             <span> Menu</span>
-            <input type="text" name="name" placeholder="Menu name">
+            <input type="text" name="name" id="name" placeholder="Menu name" >
         </div>
 
         <div class="inputBox">
             <span> Price</span>
-            <input type="text" name="price" placeholder="0.00 ">
+            <input type="decimal" name="price" placeholder="0.00 ">
         </div>
 
         <div class="inputBox">
@@ -38,19 +34,14 @@
 
         <div class="inputBox">
             <span> Image</span>
-            <input type="file" name="image" placeholder="Select image">
+            <input type="URL" name="image" id="image"placeholder="Select image" >
         </div>
 
         <div class="mb-3">
             <label for="exampleFormControlTextarea1" class="form-label">Description</label>
-            <textarea class="form-control" name="description" id="description" rows="3"></textarea>
+            <textarea class="form-control" name="description" id="description" rows="3" ></textarea>
             </div>
-
-        
-        
-        
-        
-        
+       
         <div class="inputBox"> 
         <input type="submit" value="submit" class="submit-btn" name="submit">
 </div>
@@ -61,8 +52,7 @@
 
 
     
-        
-        
+<!--end of form validation-->
 
     <?php 
     if (isset($_POST['submit'])){
@@ -78,7 +68,7 @@
       
 
       if ($conn ->query($sql)==TRUE){
-        echo "Your information is added successfully";
+        echo "Successfully Registered!";
     }
 
     else {
@@ -87,5 +77,46 @@
         
     }
     ?>
+
+<!-- Validate the form data-->
+<script type = "text/javascript">
+function formValidation(){  
+
+//validating name of menu
+let Mname=document.form1.name.value;  
+// to check if name is empty of not  
+if (Mname==null || Mname==""){  
+    alert("Name of the menu can't be blank");  
+  return false;  
+}
+
+// to check if the price field is empty of not 
+let pricechk=document.form1.price.value;  
+ 
+if (pricechk==null || pricechk==""){  
+    alert("Price can not be blank");  
+  return false;  
+}
+ else if(isNaN(pricechk)){
+    alert("Enter decimal value");  
+  return false;
+ }
+
+ // to check if the URL field is empty of not
+ let urlchk=document.form1.image.value;
+    if (urlchk==null || urlchk==""){  
+        alert("Compulsory field");  
+        return false;  
+      }
+// to check if the Description field is empty of not
+let descchk=document.form1.description.value;
+    if (descchk==null || urdescchklchk==""){  
+        alert("Compulsory field");  
+        return false;  
+      }
+    }
+  
+</script>
+
 
 <?php include "footer1.php" ?>
